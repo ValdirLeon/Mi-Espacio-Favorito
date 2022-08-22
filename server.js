9355fa1250af94a7593e99eea31efb6cd814ac3b
Valdir León <veleaoa04@gmail.com>
2022-08-22 12:50:55 -0500

    Creación de server.js y config.env

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const morgan = require('morgan')
+const dotenv = require('dotenv')
+const bodyparser = require('body-parser')
+const path = require('path')
+
+const app = express()
+
+dotenv.config({path:'config.env'})
+const PORT = process.env.PORT || 8080
+
+app.use(morgan('tiny'))
+
+app.use(bodyparser.urlencoded({extended:true}))
+
+app.set("view engine","ejs")
+
+app.use('/css',express.static(path.resolve(__dirname,"assets/css")))
+app.use('/images',express.static(path.resolve(__dirname,"assets/images")))
+app.use('/js',express.static(path.resolve(__dirname,"assets/js")))
+app.use('/actividades',express.static(path.resolve(__dirname,"assets/images/actividades")))
+app.use('/cartas',express.static(path.resolve(__dirname,"assets/images/cartas")))
+app.use('/logos',express.static(path.resolve(__dirname,"assets/images/logos")))
+app.use('/partes',express.static(path.resolve(__dirname,"assets/images/partes")))
+
+app.get('/',(req,res)=>{
+    res.send("Mi Espacio Favorito")
+})
+
+app.listen(PORT,()=>{(console.log(`Servidor funcionando en http//localhost:${PORT}`))})
